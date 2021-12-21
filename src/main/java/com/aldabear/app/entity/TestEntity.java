129d9e71a85ff872926528a569e361aefcbc48c3
Alejandro Betanciurt <administrador@softdesignermonteria.net>
2021-12-21 11:44:33 -0500

    primer commmit

---
+++ src/main/java/com/aldabear/app/entity/TestEntity.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.aldabear.app.entity;
+
+/**
+ * @author alejandro betancourt
+ *
+ */
+public class TestEntity {
+	
+	/**
+	 * Prueba de Test Entity
+	 */
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
